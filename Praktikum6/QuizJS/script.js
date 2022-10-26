8c1b7d377d56886bac9573f76e5966e74ae88b5a
Diaz-adrianz <diazadrianz191205@gmail.com>
2022-10-26 08:54:57 +0700

    Menyelesaikan praktikum 7

--- Praktikum6/QuizJS/script.js
+++ Praktikum6/QuizJS/script.js
@@ -1,35 +1,53 @@
 const output = document.getElementById('output');
+const operations = [];
 
 function clearOutput() {
 	output.textContent = 0;
 }
 
 function setNumber(val) {
+	let last = output.textContent[output.textContent.length - 1];
 	if (
 		isNaN(val) &&
 		!Number.isInteger(parseFloat(val)) &&
-		isNaN(output.textContent[output.textContent.length - 1]) &&
-		!Number.isInteger(
-			parseFloat(output.textContent[output.textContent.length - 1])
-		)
+		isNaN(last) &&
+		!Number.isInteger(parseFloat(last)) &&
+		last != ')'
 	) {
-		return;
+		if (val != '(') return;
 	}
 
-	if (output.textContent == 0) output.textContent = '';
+	if (output.textContent == 0 || output.textContent == 'error')
+		output.textContent = '';
 
 	output.textContent += val;
 }
 
 function backspace() {
+	if (output.textContent == 'error') {
+		output.textContent = '0';
+		return;
+	}
+
 	let val = output.textContent.split('');
 
 	val.pop();
-	output.textContent = val;
+
+	output.textContent = val.join('');
+
+	if (val.length == 0) output.textContent = 0;
 }
 
 function runIt() {
 	let val = output.textContent;
 
-	output.textContent = eval(val);
+	output.classList.add('fade');
+	setTimeout(() => {
+		output.classList.remove('fade');
+	}, 550);
+	try {
+		output.textContent = eval(val);
+	} catch (err) {
+		output.textContent = 'error';
+	}
 }
